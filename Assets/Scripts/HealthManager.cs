baa212ba5423dad7f951c1b3dd23a6ac0da2ac7a
WingNye <wingchunnye@gmail.com>
2022-10-10 13:40:10 -0700

    Added platforms and health bars for two players
    Added platforms and health bars for two players

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public Image playerOneHealthBar;
+    public float playerOneHealthAmount = 100f;
+
+    public Image playerTwoHealthBar;
+    public float playerTwoHealthAmount = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playerOneTakeDamage(float damage)
+    {
+        playerOneHealthAmount -= damage;
+        playerOneHealthBar.fillAmount = playerOneHealthAmount/100f;
+    }
+
+}
